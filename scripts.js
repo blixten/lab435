52c24c58fc227e392c275dc0bce059689b32f540
blixten <fredrik@fredrikbergstrand.se>
2018-11-13 22:31:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function switchContent(contentId) {
   $('#contentDiv').fadeOut( "slow", function() {
     var $contents = $(contentId).contents().clone();  // copy the source element's contents
+    $contents.css('visibility', 'visible')
     $('#contentDiv').empty().append($contents); 
   }).fadeIn(400);
 }
